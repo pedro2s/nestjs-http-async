2428aa2387f1ef88bd42cff8358aaac71adeb9bd
Pedro Santana <pedromanu27@gmail.com>
2024-12-05 18:18:23 -0300

    feat: :sparkles: add head and options methods

--- src/http-async/http-async.service.ts
+++ src/http-async/http-async.service.ts
@@ -9,9 +9,9 @@ export interface CustomAxiosRequestConfig extends CreateAxiosDefaults{
 export class HttpAsyncService {
     private axiosInstance: AxiosInstance;
 
-    constructor(private readonly options: CustomAxiosRequestConfig) {
-        const { enableLogging, ...config } = this.options;
-        this.axiosInstance = axios.create(config);
+    constructor(private readonly config: CustomAxiosRequestConfig) {
+        const { enableLogging, ...axiosConfig } = this.config;
+        this.axiosInstance = axios.create(axiosConfig);
 
         if(enableLogging) {
             this.axiosInstance.interceptors.request.use(this.handleRequest);
@@ -53,5 +53,13 @@ export class HttpAsyncService {
 
     async delete<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
         return this.axiosInstance.delete<T>(url, config);
+    }
+
+    async head<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.axiosInstance.head<T>(url, config);
+    }
+
+    async options<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.axiosInstance.options<T>(url, config);
     }
 }
